fae51e24a58ef87f72d16017c361989252e32ae3
System Administrator <root@Duncan.local>
2019-12-10 17:49:51 +0800

    test

---
+++ src/main/java/com/example/mockito/demo/config/ProductServiceTestConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.mockito.demo.config;
+
+import com.example.mockito.demo.service.ProductService;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+public class ProductServiceTestConfiguration {
+    @Bean
+    @Primary
+    public ProductService productService() {
+        return Mockito.mock(ProductService.class);
+    }
+}
